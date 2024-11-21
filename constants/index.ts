a019896dd7ca6657733b92b7b7c4bcb4258b7ae1
B L <67735440+123BL321@users.noreply.github.com>
2024-11-21 11:58:22 -0500

    Add files via upload

---
+++ constants/index.ts
@@ -0,0 +1,24 @@
+import connectscreen from "@/assets/images/connectscreen.png";
+import onboarding2 from "@/assets/images/onboarding2.png";
+
+export const images = {
+  connectscreen,
+  onboarding2
+};
+
+export const onboarding = [
+  {
+    id: 1,
+    title: "Strengthen connections and forge new ones.",
+    description:
+      "Your journey begins with Michigan Connect. Find things to do and people to do them with easier.",
+    image: connectscreen,
+  },
+  {
+    id: 2,
+    title: "Best car in your hands with Ryde",
+    description:
+      "Discover the convenience of finding your perfect ride with Ryde",
+    image: images.onboarding2,
+  },
+];
