19f9a3c8e8de97936968930e5c2cdbfbf4774cee
B L <67735440+123BL321@users.noreply.github.com>
2024-12-16 21:55:30 -0500

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,9 +1,64 @@
 import connectscreen from "@/assets/images/connectscreen.png";
 import onboarding2 from "@/assets/images/onboarding2.png";
 
+import arrowDown from "@/assets/icons/arrow-down.png";
+import arrowUp from "@/assets/icons/arrow-up.png";
+import backArrow from "@/assets/icons/back-arrow.png";
+import chat from "@/assets/icons/chat.png";
+import checkmark from "@/assets/icons/check.png";
+import close from "@/assets/icons/close.png";
+import dollar from "@/assets/icons/dollar.png";
+import email from "@/assets/icons/email.png";
+import eyecross from "@/assets/icons/eyecross.png";
+import google from "@/assets/icons/google.png";
+import home from "@/assets/icons/home.png";
+import list from "@/assets/icons/list.png";
+import lock from "@/assets/icons/lock.png";
+import map from "@/assets/icons/map.png";
+import marker from "@/assets/icons/marker.png";
+import out from "@/assets/icons/out.png";
+import person from "@/assets/icons/person.png";
+import pin from "@/assets/icons/pin.png";
+import point from "@/assets/icons/point.png";
+import profile from "@/assets/icons/profile.png";
+import search from "@/assets/icons/search.png";
+import selectedMarker from "@/assets/icons/selected-marker.png";
+import star from "@/assets/icons/star.png";
+import target from "@/assets/icons/target.png";
+import to from "@/assets/icons/to.png";
+import history from "@/assets/icons/history.png";
+
 export const images = {
   connectscreen,
   onboarding2
+};
+export const icons = {
+  arrowDown,
+  arrowUp,
+  backArrow,
+  chat,
+  checkmark,
+  close,
+  dollar,
+  email,
+  eyecross,
+  google,
+  home,
+  list,
+  lock,
+  map,
+  marker,
+  out,
+  person,
+  pin,
+  point,
+  profile,
+  search,
+  selectedMarker,
+  star,
+  target,
+  to,
+  history,
 };
 
 export const onboarding = [
@@ -16,9 +71,16 @@ export const onboarding = [
   },
   {
     id: 2,
-    title: "Best car in your hands with Ryde",
+    title: "Michigan Connect is a new way to hangout.",
     description:
-      "Discover the convenience of finding your perfect ride with Ryde",
-    image: images.onboarding2,
+      "No more scheduling conflicts with your friends or feeling isolated from events on campus.",
+    image: connectscreen,
+  },
+  {
+    id: 3,
+    title: "Michigan Connect is a new way to hangout.",
+    description:
+      "1) Share your location with your friends. 2) Plan events. 3) Stay Connected.",
+    image: connectscreen,
   },
 ];
